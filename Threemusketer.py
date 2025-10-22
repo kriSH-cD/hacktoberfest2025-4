f33ea8c0f49232911eac3e6c1e15c185140621c8
Gojo_ <krishkumar5478@gmail.com>
2025-10-22 21:50:35 +0530

    Implement Threemusketer logic for input comparison

---
+++ Threemusketer.py
@@ -0,0 +1,17 @@
+a=input()
+b=input()
+c=input()
+if a==">" and b==">" and c==">":
+    print ("Kunal")
+elif  a=="<" and b=="<" and c=="<":
+    print ("Kunal")
+elif  a=="<" and b=="<" and c==">":
+    print ("Animay")
+elif  a==">" and b==">" and c=="<":
+    print ("Animay")
+elif  a=="<" and b==">" and c==">":
+    print ("Gladden")
+elif  a==">" and b=="<" and c=="<":
+    print ("Gladden")
+else:
+    print("Impossible")
